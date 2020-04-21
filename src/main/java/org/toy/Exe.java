1263b801e170032f2e033c66f78dbb7bce64dde7
johanneshaertel <johanneshaertel@uni-koblenz.de>
2020-04-21 16:24:41 +0200

    This is a nice exe

---
+++ src/main/java/org/toy/Exe.java
@@ -0,0 +1,10 @@
+package org.toy;
+
+public class Exe {
+
+    public void exe() {
+        for (int e = 0; e < 100; e++) {
+            System.out.println("https://www.youtube.com/watch?v=WXQCo9fLYWI");
+        }
+    }
+}
