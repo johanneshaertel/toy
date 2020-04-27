81936627f8d4c4025bd69f57fa530963dd51301a
johanneshaertel <johanneshaertel@uni-koblenz.de>
2020-04-27 15:00:53 +0200

    add some comments

--- src/main/java/org/toy/MyFooAndBarRemover.java
+++ src/main/java/org/toy/MyFooAndBarRemover.java
@@ -5,10 +5,14 @@ import java.util.List;
 public class MyFooAndBarRemover {
 
     public static void removeFoo(List<String> list) {
+        // Added single line comment.
         list.removeIf(s -> s.contains("foo"));
     }
 
     public static <T> void removeBar(List<T> list) {
+        /*
+          Added multiline comment.
+         */
         list.removeIf(s -> s.toString().contains("bar"));
     }
 
