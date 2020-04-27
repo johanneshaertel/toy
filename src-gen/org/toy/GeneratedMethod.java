385a80e6c138c617c261501dfa7cb7102f271aa3
johanneshaertel <johanneshaertel@uni-koblenz.de>
2020-04-27 14:53:26 +0200

    accidentially

---
+++ src-gen/org/toy/GeneratedMethod.java
@@ -0,0 +1,8 @@
+package org.toy;
+
+public class GeneratedMethod {
+
+    public static void generated(String x) {
+        System.out.println("generated " + x);
+    }
+}
--- src/main/java/org/toy/Exe.java
+++ src/main/java/org/toy/Exe.java
@@ -4,7 +4,7 @@ public class Exe {
 
     public void exe() {
         for (int e = 0; e < 100; e++) {
-            System.out.println("https://www.youtube.com/watch?v=WXQCo9fLYWI");
+            GeneratedMethod.generated("https://www.youtube.com/watch?v=WXQCo9fLYWI");
         }
     }
 }
